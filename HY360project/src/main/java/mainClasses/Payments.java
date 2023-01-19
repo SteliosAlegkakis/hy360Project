84d2134de7a6fabbe9fa19a9b07ffb983239d75f
LAPTOP-TSM7T9MT\irene <irene.patsoura@gmail.com>
2023-01-19 13:52:35 +0200

    Added more attributes, added new constructor and added new setters and getters

--- HY360project/src/main/java/mainClasses/Payments.java
+++ HY360project/src/main/java/mainClasses/Payments.java
@@ -2,18 +2,33 @@ package mainClasses;
 
 public class Payments {
     int bill_id,emp_id;
-    double amount;
+    double amount, family_allowance, research_allowance, lib_allowance;
     String date,employee_category;
+
+    public Payments(int bill_id, int emp_id, String date, double amount, double family_allowance) {
+        this.bill_id = bill_id;
+        this.emp_id = emp_id;
+        this.date = date;
+        this.amount = amount;
+        this.family_allowance = family_allowance;
+    }
 
     public void setBillId(int bill_id){this.bill_id=bill_id;}
     public void setAmount(double amount){this.amount=amount;}
     public void setEmpId(int emp_id){this.emp_id=emp_id;}
     public void setDate(String date){this.date=date;}
     public void setEmployeeCategory(String emp_category){this.employee_category=emp_category;}
+    public void setResearch_allowance(double research_allowance){this.research_allowance=research_allowance;}
+    public void setLib_allowance(double lib_allowance) {this.lib_allowance = lib_allowance; }
+    public void setFamily_allowance(double family_allowance) { this.family_allowance = family_allowance; }
 
     public int getBillId(){return this.bill_id;}
     public double getAmount(){return this.amount;}
     public int getEmpId(){return this.emp_id;}
     public String getDate(){return this.date;}
     public String getEmployeeCategory(){return this.employee_category;}
+    public double getFamily_allowance() {return family_allowance; }
+    public double getLib_allowance() {return lib_allowance; }
+    public double getResearch_allowance() {return research_allowance;}
 }
+
